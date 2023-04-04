bbf530b290462185bfdded6c008e93befbe051f6
Vladimir Schneider <vladimir.schneider@gmail.com>
2023-04-04 11:10:43 -0400

    add gooey.h so it is recognized as a library

---
+++ src/gooey.h
@@ -0,0 +1,17 @@
+//
+// Created by Vladimir Schneider on 2023-04-04.
+//
+
+#ifndef GOOEY_GOOEY_H
+#define GOOEY_GOOEY_H
+
+#include "Events.h"
+#include "ButtonHandler.h"
+#include "InterfaceHandler.h"
+#include "InterfaceManager.h"
+#include "PopupOptionsMenu.h"
+#include "PopupMessage.h"
+#include "ConfigForm.h"
+#include "FieldEditor.h"
+
+#endif //GOOEY_GOOEY_H
